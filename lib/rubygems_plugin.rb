4b4d1413978554e9e35bcaf5d0ce13a433fafbf7
Akira Matsuda <ronnie@dio.jp>
2017-12-21 09:35:04 +0900

    Old versions of RubyGems (e.g. 2.4) have_no_have_extensions?

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -5,7 +5,11 @@ require 'rubygems'
 # move the installed gem to the shared dir and symlink to it
 Gem.post_install do |installer|
   spec = installer.spec
-  next true if spec.send(:have_extensions?)
+  if spec.respond_to? :have_extensions?
+    next true if spec.send :have_extensions?
+  else
+    next true unless spec.extensions.empty?
+  end
 
   FileUtils.mkdir_p File.expand_path('~/.gem/rbenv_shared')
   shared_gem_path = File.expand_path "~/.gem/rbenv_shared/#{spec.full_name}"
