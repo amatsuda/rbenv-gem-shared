9adb1f3ffd473f3ee47b12318e08a47bb0a97828
Akira Matsuda <ronnie@dio.jp>
2022-01-24 10:15:35 +0900

    Skip sharing gems that are included in current Git repo

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -2,6 +2,22 @@
 
 require 'rubygems'
 
+using Module.new {
+  refine Object do
+    def git_repo?(dir = Dir.pwd)
+      `cd #{dir} && git rev-parse --is-inside-work-tree 2> /dev/null`.chomp == 'true'
+    end
+
+    def git_first_commit(dir = Dir.pwd)
+      `cd #{dir} && git rev-list HEAD | tail -1`.chomp
+    end
+
+    def git_last_commit(dir = Dir.pwd)
+      `cd #{dir} && git rev-list --reverse HEAD | tail -1`.chomp
+    end
+  end
+}
+
 # move the installed gem to the shared dir and symlink to it
 Gem.post_install do |installer|
   spec = installer.spec
@@ -17,6 +33,9 @@ Gem.post_install do |installer|
   if defined?(Bundler) && Bundler.definition && (bundler_definition = Bundler.definition.dependencies.detect {|d| d.name == spec.name })
     next true if bundler_definition.source.is_a? Bundler::Source::Path
   end
+
+  # Skip if current dir and the target dir are inside same Git repo
+  next true if git_repo? && git_repo?(spec.full_gem_path) && (git_first_commit == git_first_commit(spec.full_gem_path)) && (git_last_commit == git_last_commit(spec.full_gem_path))
 
   fileutils = defined?(::Bundler::FileUtils) ? ::Bundler::FileUtils : FileUtils
 
